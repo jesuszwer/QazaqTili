7559f38d5b1db2e823ff5feb0e11d6670db2a4b7
jezuszwer <zakianovd001@gmail.com>
2024-04-12 00:44:28 +0500

    ошибка 500 исправлена

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,15 @@
 class HomeController < ApplicationController
   def index
-    @results = TestResult.where(user_id: current_user.id).order(created_at: :desc).limit(5)
-    unless @results.empty?
-      @tests = Test.where(id: @results.pluck(:test_id))
+    @results = TestResult.where(user_id: current_user.id).order(created_at: :desc).limit(3)
+    puts @results
+    unless @results.nil?
+      @tests = Test.all
+    else
+      @tests = []
+      @results = []
     end
   end
+
 
 
   private
